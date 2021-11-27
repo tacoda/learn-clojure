927bc8b6076ae98af8ee8df9d18d01207f9204a3
tacoda <tacoda@hey.com>
2021-11-27 17:21:48 -0500

    Add first part of clojure scripts and update structure

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Learn Clojure
 
 [Clojure](https://clojure.org/) is a Lisp that runs on the Java Runtime.
+
+---
+
+```bash
+clj -M 01-very-basics.clj
+```
+
+```bash
+lein repl
+```
---
+++ getting-clojure/01-hello-clojure.clj
@@ -0,0 +1,24 @@
+(println ":::The Very Basics:::")
+
+(println "Hello, world!")      ; Say hi
+
+(println (str "Clo" "jure"))
+(println (str "Hello," " " "world" "!"))
+(println (str 3 " " 2 " " 1 " Blast off!"))
+
+(println (count "Hello, world"))
+(println (count "Hello"))
+(println (count ""))
+
+(println true)
+(println false)
+
+(println "Nobody's home: " nil)
+(println "We can print many things: " true false nil)
+
+(println ":::Arithmetic:::")
+
+(println (+ 1900 84))
+(println (* 16 124))
+(println (- 2000 16))
+(println (/ 25792 13))
